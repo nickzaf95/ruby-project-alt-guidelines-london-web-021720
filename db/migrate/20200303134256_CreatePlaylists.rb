6b97d3ce095133e973b30d06fe0bee1e4d158392
nickzaf95 <nicholaszafiriou@gmail.com>
2020-03-03 14:00:11 +0000

    following by user update

--- db/migrate/20200303134256_CreatePlaylists.rb
+++ db/migrate/20200303134256_CreatePlaylists.rb
@@ -3,6 +3,7 @@ class CreatePlaylists < ActiveRecord::Migration[5.2]
     create_table :playlists do |t|
       t.string :name
       t.integer :user_id
+      t.integer :followers_user_id
     end
   end
 end
