0a0c3143e48957b5ccea92b55a0d870453f84432
nickzaf95 <nicholaszafiriou@gmail.com>
2020-03-04 12:52:17 +0000

    added capability to add a new song by user

--- lib/song.rb
+++ lib/song.rb
@@ -3,4 +3,12 @@ class Song < ActiveRecord::Base
     belongs_to :genre
     has_many :playlists, through: :playlist_joiners
 
+    def self.new_song(title, a, g)
+        # After prompting users for data, initialises a new song
+        art = Artist.create(name: a)
+        gen = Genre.create(name: g)
+        Song.create(name: title, artist: a, genre: g)
+    end
+
+
 end
